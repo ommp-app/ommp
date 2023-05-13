756afdc24a9ca8cb59c3210c04af6d527f7f83f3
ommp-app <contact@ommp.app>
2023-05-13 16:02:41 +0200

    Disable cache for prepared media

--- core/module.php
+++ core/module.php
@@ -176,11 +176,18 @@ function module_media($media, $prepare) {
     header("Content-type: $mime");
 
     // Set the cache control
-    $seconds_to_cache = intval($config->get('ommp.cache_lifetime'));
-    $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
-    header("Expires: $ts");
-    header("Pragma: cache");
-    header("Cache-Control: max-age=$seconds_to_cache");
+	if ($prepare) {
+		header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+		header("Cache-Control: post-check=0, pre-check=0", false);
+		header("Pragma: no-cache");
+		header('Expires: Tue, 17 Sep 1996 00:00:00 GMT');
+	} else {
+		$seconds_to_cache = intval($config->get('ommp.cache_lifetime'));
+		$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+		header("Expires: $ts");
+		header("Pragma: cache");
+		header("Cache-Control: max-age=$seconds_to_cache");
+	}
 
 	if ($prepare) {
 
