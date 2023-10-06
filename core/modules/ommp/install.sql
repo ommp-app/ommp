3390c61fb961680d13c394485675693419a205a7
ommp-app <contact@ommp.app>
2023-10-06 23:50:44 +0200

    Remove table drop in installation files to prevent problems when using them for updates

--- core/modules/ommp/install.sql
+++ core/modules/ommp/install.sql
@@ -13,14 +13,12 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 -- Creates the config table
-DROP TABLE IF EXISTS `{PREFIX}config`;
 CREATE TABLE IF NOT EXISTS `{PREFIX}config` (
   `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `value` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Creates the groups table
-DROP TABLE IF EXISTS `{PREFIX}groups`;
 CREATE TABLE IF NOT EXISTS `{PREFIX}groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -34,7 +32,6 @@ INSERT INTO `{PREFIX}groups` (`id`, `name`, `description`) VALUES
 (3, '{L:VISITORS}', '{L:VISITORS_DESCRIPTION}');
 
 -- Creates the groups_members table
-DROP TABLE IF EXISTS `{PREFIX}groups_members`;
 CREATE TABLE IF NOT EXISTS `{PREFIX}groups_members` (
   `group_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL
@@ -44,7 +41,6 @@ INSERT INTO `{PREFIX}groups_members` (`group_id`, `user_id`) VALUES
 (3, 0);
 
 -- Creates the modules table
-DROP TABLE IF EXISTS `{PREFIX}modules`;
 CREATE TABLE IF NOT EXISTS `{PREFIX}modules` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -54,7 +50,6 @@ CREATE TABLE IF NOT EXISTS `{PREFIX}modules` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Creates the rights table
-DROP TABLE IF EXISTS `{PREFIX}rights`;
 CREATE TABLE IF NOT EXISTS `{PREFIX}rights` (
   `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `group_id` int(11) NOT NULL,
